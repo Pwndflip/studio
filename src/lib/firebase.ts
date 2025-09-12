2d86667954868a4f9f34dc9b37170b13906975cc
I am Crewmate <egulerdinc@gmail.com>
2025-09-12 21:39:43 +0000

    hier ist mein key: const firebaseConfig = {   apiKey: "AIzaSyCCjU_9Rhscc

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,15 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-9752766556-ba34f",
-  "appId": "1:311216869920:web:eba73040a1ba69882da245",
-  "storageBucket": "studio-9752766556-ba34f.firebasestorage.app",
-  "apiKey": "AIzaSyCCjU_9RhsccnUwNN2FnwH7OLt2O22T7bU",
-  "authDomain": "studio-9752766556-ba34f.firebaseapp.com",
-  "databaseURL": "https://studio-9752766556-ba34f-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyCCjU_9RhsccnUwNN2FnwH7OLt2O22T7bU",
+  authDomain: "studio-9752766556-ba34f.firebaseapp.com",
+  databaseURL: "https://studio-9752766556-ba34f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "studio-9752766556-ba34f",
+  storageBucket: "studio-9752766556-ba34f.firebasestorage.app",
+  messagingSenderId: "311216869920",
+  appId: "1:311216869920:web:eba73040a1ba69882da245"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
